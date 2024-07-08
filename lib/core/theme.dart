cb91b8d5d2324d8821ad8205caf386f666dae2bb
Zeref101 <shreyas.mohanty03@gmail.com>
2024-07-08 14:24:31 +0530

    refactor: Update dark theme mode in AppTheme
    This commit updates the dark theme mode in the AppTheme class. The scaffold background color is set to Pallet.backgroundColor, and the text theme is updated with new styles for displayLarge, displayMedium, and displaySmall.

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:next_stop/core/theme/app_pallet.dart';
 
 class AppTheme {
-  static final darkThemeMode = ThemeData.dark()
-      .copyWith(scaffoldBackgroundColor: Pallet.backgroundColor);
+  static final ThemeData darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallet.backgroundColor,
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+      displayMedium: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+      displaySmall: TextStyle(fontSize: 14.0, fontFamily: 'Poppins'),
+    ),
+  );
 }
