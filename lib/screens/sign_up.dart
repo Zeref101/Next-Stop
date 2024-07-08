3ac7aa43fe761fd24dbdfe7a31d418017efa59c7
Zeref101 <shreyas.mohanty03@gmail.com>
2024-07-08 14:24:47 +0530

    chore: Add sign up form to SignUpScreen

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:next_stop/core/theme/app_pallet.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -8,16 +9,119 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreen extends State<SignUpScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _handleSubmit() {
+    // Step 4: Print or use the input values
+    print('Username: ${_usernameController.text}');
+    print('Email: ${_emailController.text}');
+    print('Password: ${_passwordController.text}');
+
+    _usernameController.clear();
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 50.0, // Width of the box
-          height: 50.0, // Height of the box
-          color: Color(0xFF15130B), // Color of the box
-        ),
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                        labelText: 'Username',
+                        labelStyle: const TextStyle(
+                            color: Pallet.primaryColor, fontFamily: 'Poppins'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            borderSide:
+                                const BorderSide(color: Pallet.primaryColor))),
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: const TextStyle(
+                            color: Pallet.primaryColor, fontFamily: 'Poppins'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            borderSide:
+                                const BorderSide(color: Pallet.primaryColor))),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: const TextStyle(
+                            color: Pallet.primaryColor, fontFamily: 'Poppins'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            borderSide:
+                                const BorderSide(color: Pallet.primaryColor))),
+
+                    obscureText: true, // Hides the password
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: _handleSubmit,
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              color: Pallet.onPrimaryColor,
+                              letterSpacing: 0.5,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        style: ButtonStyle(
+                            backgroundColor:
+                                WidgetStateProperty.all(Pallet.primaryColor),
+                            overlayColor: WidgetStateProperty.all(
+                                Pallet.onHoldButtonColor.withOpacity(0.5))),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
-    );
+    ));
   }
 }
